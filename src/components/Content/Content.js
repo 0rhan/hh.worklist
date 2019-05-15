75094257434d1aa21929c4241306b11081aa0775
arkhan-dev <arkhan.dev@gmail.com>
2019-05-15 18:41:22 +0300

    Анимация загрузки.
    Анимация загрузки, пока запрос не вернет вакансии.

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,16 +1,33 @@
 import React from "react";
 import { DataConsumer } from "utils/context";
 import VacancyList from "./VacancyList/VacancyList";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import styled from "styled-components";
 
 const Content = () => {
   /* Cписок вакансий */
   return (
     <DataConsumer>
-      {({ itemsCollection }) => (
-        <VacancyList itemsCollection={itemsCollection} />
-      )}
+      {({ itemsCollection, loading }) =>
+        // Отображать загрузку пока коллекция не будет загружена
+        loading ? (
+          // Анимация загрузки
+          <Loader size={50} />
+        ) : (
+          // Вакансии
+          <VacancyList itemsCollection={itemsCollection} />
+        )
+      }
     </DataConsumer>
   );
 };
 
 export default Content;
+
+const Loader = styled(CircularProgress)`
+  && {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+  }
+`;
